4cc7117172135c730a6472ca1230bc868ba5d18c
dugalcedo <107317025+dugalcedo@users.noreply.github.com>
2024-01-17 18:45:30 +0100

    dist

--- controllers/router.js
+++ controllers/router.js
@@ -1,8 +1,14 @@
+import path from 'path'
+
 import express from 'express'
 const router = express.Router()
 
 // ### Controllers ### //
 import userController from './auth/user-controller.js'
+
+router.get('/', (req, res) => {
+    res.sendFile('../frontend/dist/index.html')
+})
 
 router.use('/api/user', userController)
 
